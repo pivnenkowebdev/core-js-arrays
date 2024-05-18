2fa4dc31570f5f6ae6b6b3f34117387f4cc44f31
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-05-18 16:35:21 +0400

    feat: isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -155,8 +155,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((str) => str.length === arr[0].length);
 }
 
 /**
